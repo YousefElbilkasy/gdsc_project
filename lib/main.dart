5b3949617ccd7a2a2f93f297a8ec0e78ad949748
Yousef Elbilkasy <yousef2004bilkasy@gmail.com>
2024-02-02 01:35:35 +0200

    knkdlcnc

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,6 @@ void main() {
   runApp(const MyApp());
 }
 
-class name extends StatelessWidget {
-  const name({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
